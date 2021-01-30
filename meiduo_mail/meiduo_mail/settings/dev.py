b935a0f3e5bffa535414e8fbaa3fc20984b29582
frank <18288650698@163.com>
2021-01-30 11:11:17 +0800

    数据库配置

--- meiduo_mail/meiduo_mail/settings/dev.py
+++ meiduo_mail/meiduo_mail/settings/dev.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'meiduo_mail.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME':'meiduo',
+        'USER':'zhangsan',
+        'PASSWORD':'123456',
+        'HOST':'localhost',
+        'PORT':3306,
     }
 }
 
